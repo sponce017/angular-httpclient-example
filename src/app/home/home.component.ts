71fbf79c86f8585e8b9fca8128c24f6b3ba36281
sponce <poncesaul014@outlook.com>
2021-01-19 23:51:11 -0600

    Update

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Subject } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
+import { takeUntil } from 'rxjs/operators';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+
+  products:Product[] = [] ;
+  destroy$: Subject<Boolean> = new Subject<Boolean>();
+  constructor(private dataService:DataService) { }
+
+  ngOnInit(){
+    this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]>) => {
+      console.log(res);
+      this.products = res.body;
+    })
+  }
+
+  ngOnDestroy(){
+      this.destroy$.next(true);
+      //Unsuscribe from the subject
+      this.destroy$.unsubscribe();
+  }
+
+  public firstPage() {
+    this.products = [];
+    this.dataService.sendGetRequestToUrl(this.dataService.first).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]>) => {
+      console.log(res);
+      this.products = res.body;
+    })
+  }
+  public previousPage() {
+
+    if (this.dataService.prev !== undefined && this.dataService.prev !== '') {
+      this.products = [];
+      this.dataService.sendGetRequestToUrl(this.dataService.prev).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]>) => {
+        console.log(res);
+        this.products = res.body;
+      })
+    }
+
+  }
+  public nextPage() {
+    if (this.dataService.next !== undefined && this.dataService.next !== '') {
+      this.products = [];
+      this.dataService.sendGetRequestToUrl(this.dataService.next).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]>) => {
+        console.log(res);
+        this.products = res.body;
+      })
+    }
+  }
+  public lastPage() {
+    this.products = [];
+    this.dataService.sendGetRequestToUrl(this.dataService.last).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<Product[]>) => {
+      console.log(res);
+      this.products = res.body;
+    })
+  }
+
+}
